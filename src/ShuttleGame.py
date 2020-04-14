1345ffae281f436c696eda028445e07d89efdef7
rbonna <ricardobonna@gmail.com>
2020-04-14 20:06:38 -0300

    Minor updates

--- src/ShuttleGame.py
+++ src/ShuttleGame.py
@@ -1,4 +1,4 @@
-import os, sys, pygame, math, time, threading
+import sys, pygame, math
 from typing import Tuple
 
 SCREEN_WIDTH = 1280
@@ -25,13 +25,23 @@ class GameObject(pygame.sprite.Sprite):
 
         self.pos = position
         self.speed = speed
+        self.speed_prev = speed
         self.angle = angle  # angle in rads
         self.angle_speed = angle_speed
+        self.angle_speed_prev = angle_speed
+
+    def update_pos(self, dt: float):
+        new_pos = ((self.speed[0] + self.speed_prev[0]) * dt / 2 + self.pos[0],
+                   (self.speed[1] + self.speed_prev[1]) * dt / 2 + self.pos[1])
+        self.set_pos(new_pos)
+        self.speed_prev = self.speed
+
+        new_angle = ((self.angle_speed + self.angle_speed_prev) * dt/2 + self.angle) % (2 * math.pi)
+        self.set_angle(new_angle)
+        self.angle_speed_prev = self.angle_speed
 
     def update(self, dt: float):
-        new_pos = (self.speed[0] * dt + self.pos[0], self.speed[1] * dt + self.pos[1])
-        self.set_pos(new_pos)
-        self.set_angle((self.angle_speed * dt + self.angle) % (2 * math.pi))
+        self.update_pos(dt)
 
         if (self.rect.left > self.area.right) or \
                 (self.rect.top > self.area.bottom) or \
@@ -74,17 +84,20 @@ class GameObject(pygame.sprite.Sprite):
 
 
 class Ship(GameObject):
-    def __init__(self, image, position, m: float = 1, I: float = 1, l: float = 1, g: float = 9.81):
+    def __init__(self, image, position, mass: float = 1.0, inertia: float = 1.0, arm_length: float = 1.0,
+                 gravity: float = 9.81):
         GameObject.__init__(self, image, position)
-        self.m = m
-        self.inertia = I
-        self.length = l
-        self.g = g
+        self.mass = mass
+        self.inertia = inertia
+        self.length = arm_length
+        self.gravity = gravity
         self.reloaded = True
         self.k = 0
 
     def fire(self):
-        self.reloaded = False
+        if self.reloaded:
+            self.reloaded = False
+            print('fire')
 
     def reload(self):
         if not self.reloaded:
@@ -94,12 +107,10 @@ class Ship(GameObject):
                 self.reloaded = True
 
     def update(self, dt: float, fd=0, fe=0):
-        new_pos = (self.speed[0] * dt + self.pos[0], self.speed[1] * dt + self.pos[1])
-        self.set_pos((new_pos[0] % SCREEN_WIDTH, new_pos[1] % SCREEN_HEIGHT))
-        self.set_angle((self.angle_speed * dt + self.angle) % (2 * math.pi))
+        self.update_pos(dt)
 
-        new_speed = ((-math.sin(self.angle) / self.m) * (fd + fe) * dt + self.speed[0],
-                     (-(math.cos(self.angle) / self.m) * (fd + fe) + self.g) * dt + self.speed[1])
+        new_speed = ((-math.sin(self.angle) / self.mass) * (fd + fe) * dt + self.speed[0],
+                     (-(math.cos(self.angle) / self.mass) * (fd + fe) + self.gravity) * dt + self.speed[1])
         self.set_speed(new_speed)
         new_angle_speed = (self.length / self.inertia * (fd - fe)) * dt + self.angle_speed
         self.set_angle_speed(new_angle_speed)
@@ -125,10 +136,10 @@ if __name__ == '__main__':
     background = pygame.image.load("./figs/earth.png")
     back_size = background.get_size()
 
-    shuttle = Ship("./figs/shuttle.png", (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2),
-                   0.2, 10, 0.7, GRAVITY)
+    shuttle = Ship("./figs/shuttle.png", position=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2),
+                   mass=0.2, inertia=10.0, arm_length=1.0, gravity=GRAVITY)
 
-    asteroid = GameObject("./figs/asteroid.png", (0.0, 0.0), speed=(30.0, 20.0), angle_speed=1.0)
+    asteroid = GameObject("./figs/asteroid.png", position=(0.0, 0.0), speed=(30.0, 20.0), angle_speed=1.0)
 
     run = True
     while run:
@@ -142,9 +153,8 @@ if __name__ == '__main__':
         keys = pygame.key.get_pressed()
         Fd = THRUST if keys[pygame.K_RIGHT] else 0
         Fe = THRUST if keys[pygame.K_LEFT] else 0
-        if keys[pygame.K_SPACE] and shuttle.reloaded:
+        if keys[pygame.K_SPACE]:
             shuttle.fire()
-            print('Fire')
         pygame.event.clear()
 
         # Update actors
